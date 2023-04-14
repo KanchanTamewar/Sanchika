7b7302ec974d8e425b40ac72dce2f4da4133a76b
KanchanTamewar <tamewarkanchan123@gmail.com>
2023-04-14 09:37:21 +0530

    Added

--- src/JobitoIndiaGit/GitDemoSanch.java
+++ src/JobitoIndiaGit/GitDemoSanch.java
@@ -4,7 +4,8 @@ public class GitDemoSanch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ System.out.println("Hello Sanch");
+ System.out.println("Hello Sanch");
 	}
 
 }
